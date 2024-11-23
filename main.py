bc088ec4f8aa5cf49a72ce8611cfbe585d257b3b
rorystaehrfokc <64915956+rorystaehrfokc@users.noreply.github.com>
2024-11-23 20:53:10 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
-from flask import Flask, render_template, request
-import io, random
+# import table
+from flask import Flask, request, redirect, render_template, make_response
+import io, json, os
+from datetime import datetime
 
-
+# Flask initialaize
 app = Flask(__name__,
             static_folder="static",
             static_url_path="/")
 
+# File to store messages
+MESSAGES_FILE = "messages.json"
+
+# Load messages from disk or initialize
+if os.path.exists(MESSAGES_FILE):
+    with open(MESSAGES_FILE, "r") as file:
+        messages = json.load(file)
+else:
+    messages = {}
+
+
+# Home
 @app.route('/')
 def home():
     return render_template('home.html',
                            title="Home")
 
+#About
 @app.route('/about')
 def about():
     return  render_template('about.html',
                             title="About")
+
 
 @app.route('/querystring')
 def quertstring():
@@ -27,13 +43,29 @@ def quertstring():
                             age=age,
                             nationality=nationality)
 
-@app.route('/msg')
-def msg():
-    messeges = [
-        
-    ]
-    return  render_template('msg.html',
-                            title="Msg")
+@app.route('/messeges')
+def messeges():
+    return  render_template('messeges.html',
+                            title="Messeges")
+
+
+@app.route("/set_cookie")
+def set_cookie():
+    response = make_response("Cookie set")
+    response.set_cookie("cookie_name", "cookie_value")
+    return response
+
+@app.route("/get_cookie")
+def get_cookie():
+    cookie_value = request.cookies.get("cookie_name")
+    return cookie_value
+
+@app.route("/delete_cookie")
+def delete_cookie():
+    response = make_response("Cookie Deleted")
+    response.delete_cookie("cookie_name")
+    return response
+
 
 
 if __name__ == '__main__':
